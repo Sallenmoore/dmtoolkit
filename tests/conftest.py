6e939ea894d2761aae45ae0f25357d5734ae7373
slgy <me@nowhere.com>
2023-06-22 13:41:05 -0400

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,9 +6,10 @@ from models.dndshop import Shop
 from dotenv import load_dotenv
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def load_env():
     load_dotenv()
+
 
 @pytest.fixture
 def monster():
